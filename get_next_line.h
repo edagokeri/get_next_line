1bdfa7f8822729dd2ac4411f98f5dff85053afb2
egokeri <112097628+egokeri@users.noreply.github.com>
2022-12-24 11:28:27 +0300

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -13,10 +13,8 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include <fcntl.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
